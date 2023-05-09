fb66882a80f860892b1860162e27192f4948675f
Loudsb <marie-lou-34@live.fr>
2023-05-09 16:43:23 +0200

    début trad en fol

--- prolog/transfo_DRT_to_FOL.pl
+++ prolog/transfo_DRT_to_FOL.pl
@@ -14,20 +14,19 @@ exemple(2,drs([],[imp_drs(drs([x,y],[fermier(x),âne(y),possede(x,y)]),drs([u,v]
 %Exemple 3 négation
 exemple(3,drs([],[non(drs([x,y],[man(x),enter(x),smiled(y),eq(y,_)]))])).
 
+% TRADUCTION DES DRS EN LOGIQUE DRT
+
 %Je crée une liste qui contient tous les référents de la DRS
 listeRef(drs(ListeR,_), ListeR).
     
 %Je crée une liste qui contient toutes les conditions
 listeCond(drs(_,ListeC),ListeC).
 
-%Traduction des référents et des conditions
+%Traduction des référents
 list_to_vars([],true).
 list_to_vars([X], exist(X)) :- !.
 list_to_vars([X|Xs], and(exist(X),E)) :- list_to_vars(Xs,E).
 
-list_to_cond([],true).
-list_to_cond([X],X) :- !.
-list_to_cond([X|Xs], and(X,E)) :- list_to_cond(Xs,E).
 
 %Je fais un and() des formules des référents et des conditions
 list_and(L1, L2, and(L1,L2)).
@@ -38,9 +37,8 @@ list_and(L1, L2, and(L1,L2)).
 %Prend 2 formules et les met dans un implique
 impl(F1,F2,imp(F1,F2)).
 
-%trad_cond([],_).
-trad_cond([X],Y) :- handle(X,Y), !.
-trad_cond([X|Xs], and(Y,R)) :- handle(X,Y), trad_cond(Xs,R).
+% Vérifie si une condition est une DRS
+is_drs(drs(_,_)).
 
 % Si la condition commence par imp et que les deux trucs sont des drs on les
 % traduit en formules logiques et on applique l'implication
@@ -53,25 +51,46 @@ handle(X,R) :- is_drs(X), !, drs_to_drt(X,R).
 %Si la condition est un prédicat basique je ne touche à rien
 handle(X,X) :- \+ is_drs(X).
 
-% Vérifie si une condition est une DRS
-is_drs(drs(_,_)).
+%trad_cond([],_).
+trad_cond([X],Y) :- handle(X,Y), !.
+trad_cond([X|Xs], and(Y,R)) :- handle(X,Y), trad_cond(Xs,R).
 
 % Transformation d'une DRS en formule logique
 drs_to_drt(DRS,R) :- listeRef(DRS,Re), listeCond(DRS,C), list_to_vars(Re,L1), trad_cond(C,L2), list_and(L1,L2,R).
 
 
-trad(X) :- exemple(X,DRS), drs_to_drt(DRS,R), format('La traduction de la drs est : ~n ~p. ~n',[R]).
+trad_drs(X,R) :- exemple(X,DRS), drs_to_drt(DRS,R), format('La traduction de la drs est : ~n ~p. ~n',[R]).
 
 
+% TRADUCTION EN LOGIQUE DU PREMIER ORDRE
+
+%On construit la liste des variables exist
+list_exist(and(exist(X),exist(Y)),[X,Y]) :- !.
+list_exist(and(exist(X),Y),[X,R]) :- Y \== exist(_), list_exist(Y,R).
+
+trad_exist_fol(Exists,Cond,exist(Exists,Cond)).
+
+get_exist_vars(exist(X), [X]) :- !.
+get_exist_vars(and(F1, F2), L) :-
+    get_exist_vars(F1, L1),
+    get_exist_vars(F2, L2),
+    append(L1, L2, L), !.
+get_exist_vars(_, []) :- !.
+
+get_cond(and(and(exist(_),_),C),[C]).
+
+drt_to_fol(not(X),not(R)) :- drt_to_fol(X,R).
+drt_to_fol(imp(X,Y),R) :- 
+    drt_to_fol(X,R1),
+    drt_to_fol(Y,R2),
+    impl(R1,R2,R).
+drt_to_fol(F,R) :-
+    get_exist_vars(F, L1),
+    get_cond(F,L2),
+    trad_exist_fol(L1,L2,R).
 
 
-
-
-
-
-
-
-
+trad_fol(X,R) :- trad_drs(X,R1), drt_to_fol(R1,R), format('La traduction en fol est : ~n').
 
 
 
