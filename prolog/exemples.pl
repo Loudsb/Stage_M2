c39c05a678799c803c0a49b0634d14a445006155
Loudsb <marie-lou-34@live.fr>
2023-06-07 19:21:00 +0200

    mise en lettre majuscule des arguments pour le format TPTP

--- prolog/exemples.pl
+++ prolog/exemples.pl
@@ -1,19 +1,24 @@
 % EXEMPLES
 
 %Exemple 1 facile
-exemple(1,drs([x,y],[man(x),enter(x),smiled(y),eq(y,x)])).
+exemple(1,drs([x,y],[man(x),enter(x),smiled(y),eq(y,ante)])).
 
 %Exemple 2 implication
 %Si un fermier possède un âne, il le bât.
 %exist(x),exist(y),impl(and(fermier(x),and(âne(y),and(posseder(x,y),true))),
-exemple(2,drs([],[imp_drs(drs([x,y],[fermier(x),âne(y),possede(x,y)]),drs([u,v],[eq(u,x),eq(v,y),battre(u,v)]))])).
+exemple(2,drs([],[imp_drs(drs([x,y],[fermier(x),âne(y),possede(x,y)]),drs([u,v],[eq(u,ante1),eq(v,ante2),battre(u,v)]))])).
 
 %Exemple 3 négation
-exemple(3,drs([],[non(drs([x,y],[man(x),enter(x),smiled(y),eq(y,x)]))])).
+exemple(3,drs([],[non(drs([x,y],[man(x),enter(x),smiled(y),eq(y,ante)]))])).
 
 %Exemple 4 négation
 %Pierre ne connait aucune fille
 exemple(4,drs([x],[pierre(x),non(drs([y],[fille(y),connait(x,y)]))])).
 
-%Exemple 5 implication
-exemple(5,drs([x],[pierre(x),imp_drs(drs([x,y],[fermier(x),âne(y),possede(x,y)]),drs([u,v],[eq(u,x),eq(v,y),battre(u,v)]))])).
+%Exemple 5
+%À chaque fois que Marie relit un livre, il l’impressionne.
+exemple(5,drs([x],[lea(x), imp_drs(drs([y],[livre(y),relit(x,y)]),drs([u,v],[impressionne(u,v),eq(u,ante1),eq(v,ante2)]))])).
+
+%Exemple 6
+%Un exemple qui n'a aucun sens pour tester différents cas de figures
+exemple(6,drs([x],[non(pierre(x)),imp_drs(drs([x,y],[fermier(x),âne(y),possede(x,y)]),drs([u,v],[eq(u,ante1),eq(v,ante2),battre(u,v)]))])).
--- prolog/transfo_FOL_to_TPTP.pl
+++ prolog/transfo_FOL_to_TPTP.pl
@@ -1,33 +1,62 @@
 % TRADUCTION AU FORMAT TPTP
 
+%prédicat qui prend une liste de variables et qui les mets en lettres majuscules
+convert_to_uppercase([], []).
+convert_to_uppercase([T|Q],[S|R]) :-
+    upcase_atom(T,S),
+    convert_to_uppercase(Q,R).
+
+%vérifie que le paramètre donné est bien un prédicat
+est_predicat(Term) :- compound(Term), functor(Term, _, Arity), Arity > 0.
+
+
 fol_to_tptp(forall(X,[Y]),R) :-
     fol_to_tptp(Y,R1),
-    format(atom(R), '!~w:~w', [X, R1]), !.
+    convert_to_uppercase(X,X1),
+    format(atom(R), '(!~w : (~w))', [X1, R1]), !.
 fol_to_tptp(exist(X,[Y]),R) :-
     fol_to_tptp(Y,R1),
-    format(atom(R), '?~w:~w', [X, R1]), !.
+    convert_to_uppercase(X,X1),
+    format(atom(R), '(?~w : (~w))', [X1, R1]), !.
 fol_to_tptp(and(F1,F2),R) :-
     fol_to_tptp(F1,R1),
     fol_to_tptp(F2,R2),
-    format(atom(R), '~w & ~w', [R1,R2]), !.
+    format(atom(R), '(~w & ~w)', [R1,R2]), !.
 fol_to_tptp(or(F1,F2),R) :-
     fol_to_tptp(F1,R1),
     fol_to_tptp(F2,R2),
-    format(atom(R), '~w | ~w', [R1,R2]), !.
+    format(atom(R), '(~w | ~w)', [R1,R2]), !.
 fol_to_tptp(imp(F1,F2),R) :-
     fol_to_tptp(F1,R1),
     fol_to_tptp(F2,R2),
-    format(atom(R), '~w => ~w', [R1,R2]), !.
+    format(atom(R), '(~w => ~w)', [R1,R2]), !.
 fol_to_tptp(eq(X,Y),R) :-
     fol_to_tptp(X,R1),
     fol_to_tptp(Y,R2),
-    format(atom(R), '~w = ~w', [R1, R2]), !.
+    format(atom(R), '(~w = ~w)', [R1, R2]), !.
 fol_to_tptp(not(F),R) :-
     fol_to_tptp(F,R1),
     format(atom(R), '~c(~w)', [126,R1]), !.
 fol_to_tptp(F,R) :-
-    format(atom(R), '~w', [F]), !.
- 
+    atom(F), %si c'est un atom
+    upcase_atom(F,U),
+    format(atom(R), '~w', [U]), !.
+fol_to_tptp(F,R) :-
+    est_predicat(F), % si c'est un prédicat
+    functor(F, Foncteur, Arity),
+    functor(R, Foncteur, Arity),
+    fol_to_tptp_params(F, R, Arity),
+    !.
+
+% Règle pour transformer les paramètres en majuscules
+fol_to_tptp_params(_, _, 0). %tous les paramètres ont été traités
+fol_to_tptp_params(F, R, N) :-
+    N > 0, %il reste des paramètres à traiter
+    arg(N, F, Parametre), %prend le N-ème paramètre du prédicat et l'assigne à la variable Parametre
+    arg(N, R, ParametreMajuscule), %prend le N-ème paramètre du résultat et l'assigne à la variable ParametreMajuscule
+    upcase_atom(Parametre, ParametreMajuscule),
+    N1 is N - 1,
+    fol_to_tptp_params(F, R, N1).
 
 trad_tptp(X,R) :-
     trad_fol_v2(X,R1),
